98d7ff77756ae89a87bf8c68f00a5585a303a486
Shion Ichikawa <shion1305@gmail.com>
2022-11-17 17:12:44 +0900

    🎉 Project Startup

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtPage/>
+    <v-icon>mdi-alert</v-icon>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>企画団体マイページへようこそ</h1>
+    <!--    <v-btn class="ma-5" color="primary"-->
+    <!--           href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz">-->
+    <!--      my-btn-->
+    <!--    </v-btn>-->
+    <!--    <nuxt-link to="/about">Go to about</nuxt-link>-->
+  </div>
+</template>
+<script setup>
+import {onBeforeMount, ref} from "vue";
+
+const showLogin = ref(false)
+const showOrgs = ref(false)
+onBeforeMount(() => {
+  console.log(document.cookie)
+})
+</script>
