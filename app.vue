0c0a6dc2368c81af746a168d6825d72732bf6012
Shion Ichikawa <shion1305@gmail.com>
2022-12-11 14:34:50 +0900

    ➕ GoogleFontの追加

--- app.vue
+++ app.vue
@@ -4,3 +4,10 @@
     <v-icon>mdi-alert</v-icon>
   </div>
 </template>
+
+<style>
+
+html {
+  font-family: 'Klee One', 'Noto Color Emoji', cursive, sans-serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     typescript: {
         shim: false
         // strict: true
     },
-    css: ['vuetify/styles'],
+    css: ['vuetify/styles', '@/assets/css/index.css'],
     build: {
         transpile: ['vuetify']
     },
