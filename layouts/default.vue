926c920bd3fe1b74247660959a0ff8ece8fe290a
tomoyahiroe <tomoya4creative@gmail.com>
2022-12-12 21:22:26 +0900

    feat: layouts/default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="dashboard-root-frame">
+    Layout: default
+    <sidebar />
+    <div id="dashboard-root-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#dashboard-root-frame {
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+}
+
+#dashboard-root-content {
+  width: calc(100vw - 200px);
+  height: 100%;
+}
+</style>
