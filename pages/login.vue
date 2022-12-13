9fd2272e11924036d0372ce71f977e5d5f3854bd
tomoyahiroe <tomoya4creative@gmail.com>
2022-12-13 12:13:11 +0900

    feat: ログイン画面のデザインを簡易的に作成

--- pages/login.vue
+++ pages/login.vue
@@ -9,15 +9,29 @@ definePageMeta({
     <v-container
       class="login-button-wrapper d-flex m-0 justify-center align-center"
     >
-      <v-btn class="login-button">LINEでログイン</v-btn>
+      <div>
+        <v-img class="cite-logo" src="/YNUFES-icon-color.png"></v-img>
+        <v-btn class="login-button"
+          ><NuxtLink to="" style="text-decoration: none">
+            LINEでログイン</NuxtLink
+          ></v-btn
+        >
+      </div>
     </v-container>
-    <v-container class="login-icon-wrapper m-0" color="primary">
+    <v-container class="login-icon-wrapper m-0">
       <v-img src="/login-page.svg" justify="center" class="w-50"></v-img>
     </v-container>
   </div>
 </template>
 
 <style scoped lang="scss">
+.cite-logo {
+  position: absolute;
+  width: 240px;
+  height: 120px;
+  top: 10%;
+  transform: translateX(5%);
+}
 .login-button {
   background-color: #06c755;
   width: 256px;
@@ -36,11 +50,11 @@ definePageMeta({
   transform: translateY(-50%);
 }
 .login-button-wrapper {
-  width: 55vw;
+  width: 45vw;
 }
 .login-icon-wrapper {
   height: 100vh;
-  width: 45vw;
+  width: 55vw;
   background-color: #92e3a9;
   display: flex;
   align-items: center;
