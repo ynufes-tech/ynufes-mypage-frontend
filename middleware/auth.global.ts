b64ef9ff861817fa6b387cd4ccb56fc3e0a3fca1
tomoyahiroe <tomoya4creative@gmail.com>
2022-12-27 16:25:49 +0900

    feat: auth.global.ts as a auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,1 +1,21 @@
-export default defineNuxtRouteMiddleware((to, from) => {})
+export default defineNuxtRouteMiddleware((to, from) => {
+  const url = "https://ynufes-mypage.shion.pro/api/v1/user/info";
+  const authToken = useRequestHeaders(["authorization"]);
+
+  if (typeof authToken !== "string") return abortNavigation();
+  // const authToken = "";
+  const headers = new Headers({
+    Authorization: authToken,
+  });
+
+  const { data, error } = useFetch(url, {
+    method: "GET",
+    headers: headers,
+  });
+  if (!data || error) {
+    navigateTo("login");
+  }
+
+  // もし、"INVALID Authorization Header"なら、loginページに返す。
+  // それ以外は何もしない
+});
