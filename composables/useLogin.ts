c54318231a846c0698a244827bc1e37069d4c4e4
Shion Ichikawa <shion1305@gmail.com>
2023-01-07 23:46:44 +0900

    ✨ (composables)User class

--- composables/useLogin.ts
+++ composables/useLogin.ts
@@ -1,42 +1,7 @@
-export const useIsLoggedIn = async () => {
-  const url = "https://ynufes-mypage.shion.pro/api/v1/user/info";
-  const cookie = useRequestHeaders(["cookie"]);
-  const prefixLength: number = "Authorization=".length;
-  const authToken = cookie?.cookie?.substring(prefixLength);
-
-  if (
-    typeof authToken !== "string" ||
-    authToken === "" ||
-    authToken === undefined ||
-    authToken === null
-  ) {
-    console.log(
-      "トークンが正しい型でないため事前にログインユーザでないと判定しました。"
-    );
-    return false;
-  }
-
-  const headers = new Headers({
-    Authorization: `Bearer ${authToken}`,
-  });
-
-  const { data, error } = await useFetch(url, {
-    method: "GET",
-    headers: headers,
-  });
-
-  if (!error || !data) {
-    console.log("バックエンドAPIから一切の情報を得られませんでした。");
-    return false;
-  }
-
-  if (error.value?.status === 401) {
-    console.log(JSON.stringify(error.value.status) + "Unauthorized");
-    return false;
-  }
-
-  if (data.value) {
-    console.log("適切なトークンです。ログインユーザと判定します。");
-    return true;
-  }
-};
+export class User {
+    name_first: string = "";
+    name_last: string = "";
+    profile_picture: string = "";
+    type: number = 0;
+    status: number = 0;
+}
