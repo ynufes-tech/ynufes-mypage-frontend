424396f91254aecbca65a401c0036df9b2353f64
Shion Ichikawa <shion1305@gmail.com>
2023-01-08 01:14:10 +0900

    ✨ (plugin)composables useLoginの適用

--- plugins/login.ts
+++ plugins/login.ts
@@ -1,23 +1,4 @@
 export default defineNuxtPlugin(async (app) => {
-    const {$config} = useRuntimeConfig()
-    const authorization = useCookie('Authorization')
-    console.log('Authorization: ' + authorization.value)
-    if (!authorization.value) {
-        console.log('no cookie')
-        return
-    }
-    const {data: post, error} = await useFetch('http://localhost:1306/api/v1/user/info', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + authorization.value,
-        }
-    })
-    if (error.value != null) {
-        console.log('401 Unauthorized')
-        console.log(error)
-        return
-    } else {
-        console.log('200 OK')
-        console.log(post)
-    }
+    const {trySignIn} = useLogin();
+    await trySignIn()
 })
