2272b616bbc0f88833955aef77cb057748572972
Shion Ichikawa <shion1305@gmail.com>
2023-01-09 17:52:58 +0900

    ✨ (middleware)詳細なrouting

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,12 +1,29 @@
 import {useLogin} from "~/composables/useLogin";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    if (to.path.startsWith('/login')) return;
     const {isSignedIn, getCurrentUser} = useLogin();
-    if (!isSignedIn()) {
-        return navigateTo('/login');
+    if (to.path.startsWith('/login')) {
+        if (!isSignedIn()) return;
+        switch (getCurrentUser().status) {
+            case 1:
+                return navigateTo('/welcome');
+            case 2:
+                return navigateTo('/');
+            default:
+                return
+        }
     }
+    if (to.path.startsWith('/welcome')) {
+        if (!isSignedIn()) return;
+        switch (getCurrentUser().status) {
+            case 1:
+                return;
+            case 2:
+                return navigateTo('/');
+        }
+    }
+    if (!isSignedIn()) navigateTo('/login');
     if (getCurrentUser().status === 1) {
         return navigateTo('/welcome');
     }
-});
+})
