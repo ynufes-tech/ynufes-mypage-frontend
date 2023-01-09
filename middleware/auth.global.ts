9d6da24019c4b2225d10af5c26ca91a022a146c8
Shion Ichikawa <shion1305@gmail.com>
2023-01-09 15:16:27 +0900

    ✨ (middleware)status===1の時に登録ページへ遷移

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,8 +2,11 @@ import {useLogin} from "~/composables/useLogin";
 
 export default defineNuxtRouteMiddleware((to, from) => {
     if (to.path.startsWith('/login')) return;
-    const {isSignedIn} = useLogin();
+    const {isSignedIn, getCurrentUser} = useLogin();
     if (!isSignedIn()) {
         return navigateTo('/login');
+    }
+    if (getCurrentUser().status === 1) {
+        return navigateTo('/welcome');
     }
 });
