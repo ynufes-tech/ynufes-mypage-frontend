dc8bb75bc34c98b82880c4406bbdb51ebb4fee2d
tomoyahiroe <tomoya4creative@gmail.com>
2023-01-09 23:29:50 +0900

    fix: authミドルウェアに軽微の修正。ログインしていないアカウントが/welcomeにアクセスした際に、/loginに飛ばす様に修正

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,29 +1,29 @@
-import {useLogin} from "~/composables/useLogin";
+import { useLogin } from "~/composables/useLogin";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const {isSignedIn, getCurrentUser} = useLogin();
-    if (to.path.startsWith('/login')) {
-        if (!isSignedIn()) return;
-        switch (getCurrentUser().status) {
-            case 1:
-                return navigateTo('/welcome');
-            case 2:
-                return navigateTo('/');
-            default:
-                return
-        }
+  const { isSignedIn, getCurrentUser } = useLogin();
+  if (to.path.startsWith("/login")) {
+    if (!isSignedIn()) return;
+    switch (getCurrentUser().status) {
+      case 1:
+        return navigateTo("/welcome");
+      case 2:
+        return navigateTo("/");
+      default:
+        return;
     }
-    if (to.path.startsWith('/welcome')) {
-        if (!isSignedIn()) return;
-        switch (getCurrentUser().status) {
-            case 1:
-                return;
-            case 2:
-                return navigateTo('/');
-        }
+  }
+  if (to.path.startsWith("/welcome")) {
+    if (!isSignedIn()) return navigateTo("/login");
+    switch (getCurrentUser().status) {
+      case 1:
+        return;
+      case 2:
+        return navigateTo("/");
     }
-    if (!isSignedIn()) navigateTo('/login');
-    if (getCurrentUser().status === 1) {
-        return navigateTo('/welcome');
-    }
-})
+  }
+  if (!isSignedIn()) navigateTo("/login");
+  if (getCurrentUser().status === 1) {
+    return navigateTo("/welcome");
+  }
+});
