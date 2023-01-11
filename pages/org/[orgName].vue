d69d032dfeddf7b57428e632f203c65aca777c68
tomoyahiroe <tomoya4creative@gmail.com>
2023-01-11 14:57:36 +0900

    style: orgPageにひとまずのフォームリンクカードコンポーネント配置

--- pages/org/[orgName].vue
+++ pages/org/[orgName].vue
@@ -1,16 +1,32 @@
 <template>
   <main class="main-content-wrapper">
-    <h1>{{ $route.params.orgName }}</h1>
-    <div class="count-down"></div>
-    <TopOrgPageLinkCard />
+    <h1 style="margin-bottom: 40px">{{ $route.params.orgName }}</h1>
+
+    <h2 style="margin-bottom: 20px">フォーム提出状況</h2>
+    <div class="cards">
+      <FormCard :status="1" title="受理済みのフォーム" />
+      <FormCard :status="2" title="提出済みのフォーム" />
+      <FormCard :status="3" title="期限内未提出のフォーム" />
+      <FormCard :status="4" title="期限間近未提出のフォーム" />
+      <FormCard :status="5" title="期限超過未提出のフォーム" />
+    </div>
   </main>
 </template>
-<script setup></script>
-<style>
+<script setup>
+import FormCard from "../../components/org/FormLinkCard";
+</script>
+<style lang="scss" scoped>
 .main-content-wrapper {
   margin: 20px 40px;
 }
 .count-down {
   margin: 40px 0;
+}
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 1000px;
+  gap: 10px;
 }
 </style>
