e8eddd5b337b6700c1293c1fd2c95bd595a452be
tomoyahiroe <tomoya4creative@gmail.com>
2023-01-14 14:49:47 +0900

    enableRouting false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,35 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    typescript: {
-        shim: false
-        // strict: true
+  typescript: {
+    shim: false,
+    // strict: true
+  },
+  css: ["vuetify/styles", "@/assets/css/index.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
     },
-    css: ['vuetify/styles', '@/assets/css/index.css'],
-    build: {
-        transpile: ['vuetify']
+    server: {
+      watch: {
+        usePolling: true,
+      },
     },
-    vite: {
-        define: {
-            'process.env.DEBUG': false
-        },
-        server: {
-            watch: {
-                usePolling: true
-            }
-        }
+  },
+  components: {
+    dirs: ["~/components", "~/components/sidebar"],
+  },
+  runtimeConfig: {
+    // only variables under public are exposed to the client side
+    // refer: https://dev.to/amitgurbani/environment-variables-in-nuxt-3-9p6
+    public: {
+      baseURL: "http://localhost:1306",
+      callbackURL: "http://localhost:1306/api/v1/auth/line/callback",
+      lineClientID: process.env.LINE_CLIENT_ID,
+      enableRouting: false,
     },
-    components: {
-        dirs: [
-            '~/components',
-            '~/components/sidebar',
-        ]
-    },
-    runtimeConfig: {
-        // only variables under public are exposed to the client side
-        // refer: https://dev.to/amitgurbani/environment-variables-in-nuxt-3-9p6
-        public: {
-            baseURL: 'http://localhost:1306',
-            callbackURL: 'http://localhost:1306/api/v1/auth/line/callback',
-            lineClientID: process.env.LINE_CLIENT_ID,
-            enableRouting: true
-        }
-    },
-    ssr: false,
-})
+  },
+  ssr: false,
+});
