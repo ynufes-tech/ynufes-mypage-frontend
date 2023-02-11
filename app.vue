42effd5f70315accc17fa2f52de2bdd68f8992d4
Gityosan <ymo6190@docomo.ne.jp>
2023-02-11 18:31:24 +0900

    [fix] fix scss

--- app.vue
+++ app.vue
@@ -1,18 +1,3 @@
 <template>
   <NuxtLayout />
 </template>
-
-<style>
-/* デバッグ用の３行------------------------- */
-/* * {
-  outline: 1px solid red !important;
-} */
-body {
-  margin: 0;
-  padding: 0;
-}
-
-html {
-  font-family: 'Klee One', 'Noto Color Emoji', sans-serif;
-}
-</style>
