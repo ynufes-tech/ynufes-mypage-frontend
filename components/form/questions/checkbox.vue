215d22b126d771abef1c9a8709eaa7b65b970c0f
tomoyahiroe <tomoya4creative@gmail.com>
2023-02-26 20:30:01 +0900

    feat: checkboxの値の取得に成功

--- components/form/questions/checkbox.vue
+++ components/form/questions/checkbox.vue
@@ -7,15 +7,23 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   question: '使用したい教室名を選択してください',
   description: '',
-  options: () => ['経済101', '教育1305', '情報基盤センター全館']
+  options: () => ['経済101', '教育1305', '情報基盤センター']
 })
+const checked = ref<boolean[]>([])
 </script>
 
 <template>
   <v-card width="100%" elevation="2">
     <v-card-title>{{ question }}</v-card-title>
     <v-card-text>{{ description }}</v-card-text>
-    <v-checkbox v-for="option in options" :label="option"></v-checkbox>
+    <div v-for="option in options">
+      <v-checkbox
+        v-model="checked"
+        :label="option"
+        :value="option"
+      ></v-checkbox>
+    </div>
+    <p>{{ checked }}</p>
   </v-card>
 </template>
 
