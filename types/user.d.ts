010fa9724ccded8c45ae736a5a53a17a94f4a8a2
Shion Ichikawa <shion1305@gmail.com>
2023-03-09 02:07:28 +0900

    ✨ (types/user)

---
+++ types/user.d.ts
@@ -0,0 +1,12 @@
+export interface User {
+  name_first: string
+  name_first_kana: string
+  name_last: string
+  name_last_kana: string
+  type: number
+  profile_icon_url: string
+  email: string
+  gender: number
+  student_id: string
+  status: number
+}
