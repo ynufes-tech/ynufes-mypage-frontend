16696a329be1d8e5af0fa18e180d1449f333002e
tomoyahiroe <tomoya4creative@gmail.com>
2024-01-11 13:44:00 +0900

    :coffin: remove: ログインページからvuetifyの要素を消す。

--- pages/login.vue
+++ pages/login.vue
@@ -22,76 +22,10 @@ const goAuthURL = async () => {
 </script>
 <template>
   <div class="d-flex m-0">
-    <v-container class="login-button-wrapper">
-      <v-img class="site-logo" src="/mypage-logo-dark.webp"></v-img>
-      <v-btn class="login-button d-flex" @click="goAuthURL()">
-        <v-img src="/line_btn_base.png" class="login-button__icon"></v-img>
-        <span>LINEでログイン</span>
-      </v-btn>
-    </v-container>
-    <v-container class="login-icon-wrapper mx-0">
-      <v-img src="/login-page.svg" class="login-icon-img"></v-img>
-    </v-container>
+    <button class="login-button d-flex" @click="goAuthURL()">
+      <img src="/line_btn_base.png" class="login-button__icon" />
+      <span>LINEでログイン</span>
+    </button>
   </div>
 </template>
-<style scoped lang="scss">
-.site-logo {
-  width: 100%;
-  height: 60px;
-  margin-top: calc((100svh - 60px - 48px - 32px - 60px) / 2);
-  margin-bottom: 60px;
-}
-
-.login-button {
-  text-decoration: none;
-  border-radius: 10px;
-  background-color: #06c755;
-  height: 48px;
-  width: fit-content;
-  color: #fff;
-  padding-right: 16px;
-  margin: 0 auto;
-  font-family: Helvetica, sans-serif;
-  font-weight: bold;
-  font-size: 1rem;
-
-  &:hover {
-    filter: opacity(0.9);
-  }
-
-  &__icon {
-    width: 40px;
-    height: 40px;
-    margin: 5px 8px 0 8px;
-  }
-
-  > span {
-    padding: 0;
-    font-family: Helvetica, sans-serif;
-    font-weight: bold;
-    display: flex;
-    align-items: center;
-    line-height: 1.5em;
-  }
-}
-
-.login-button-wrapper {
-  width: 45vw;
-  max-width: 520px;
-  flex: 0 0 45vw;
-}
-
-.login-icon-wrapper {
-  height: 100svh;
-  width: 55vw;
-  flex: 1;
-  background-color: #92e3a9;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.login-icon-img {
-  max-height: 480px;
-}
-</style>
+<style scoped lang="scss"></style>
