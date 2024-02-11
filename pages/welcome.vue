ea751698673a51d1d4233d0655403e1bba94a82f
tomoyahiroe <tomoya4creative@gmail.com>
2024-02-12 01:28:20 +0900

    add: sample code

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { validation } from '@/assets/validation'
 useHead({ title: 'welcome' })
 definePageMeta({
   layout: false
@@ -11,112 +10,96 @@ const firstNameKana = ref<string>('')
 const lastNameKana = ref<string>('')
 const email = ref<string>('')
 const gender = ref<string>('')
-const studentID = ref<string>('')
+const studentID = ref<number>()
+const selectedCity = ref()
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' }
+])
+const checked = ref<boolean>(false)
 </script>
 <template>
-  <v-container class="mx-auto">
-    <h1 class="welcome-title mb-10">初期情報登録画面</h1>
-    <v-row
-      ><v-col col="12" sm="12" md="12" lg="10">
-        <v-card elevation="10">
-          <v-form v-model="valid">
-            <v-container>
-              <v-card-title>お名前</v-card-title>
-              <v-row>
-                <v-col cols="12" xs="12" sm="6" md="6" lg="6">
-                  <v-text-field
-                    v-model.trim="lastName"
-                    label="名字"
-                    placeholder="市川"
-                    type="text"
-                    required
-                    :rules="[validation.required, validation.maxString(10)]"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" xs="12" sm="6" md="6" lg="6">
-                  <v-text-field
-                    v-model.trim="firstName"
-                    label="名前"
-                    placeholder="詩恩"
-                    type="text"
-                    required
-                    :rules="[validation.required, validation.maxString(10)]"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-card-title>フリガナ</v-card-title>
-              <v-row>
-                <v-col cols="12" xs="12" sm="6" md="6" lg="6">
-                  <v-text-field
-                    v-model.trim="lastNameKana"
-                    label="ミョウジ"
-                    placeholder="イチカワ"
-                    type="text"
-                    required
-                    :rules="[validation.required, validation.maxString(10)]"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" xs="12" sm="6" md="6" lg="6">
-                  <v-text-field
-                    v-model.trim="firstNameKana"
-                    label="ナマエ"
-                    placeholder="シオン"
-                    type="text"
-                    required
-                    :rules="[validation.required, validation.maxString(10)]"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-card-title>メールアドレス</v-card-title>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model.trim="email"
-                    label="メールアドレス"
-                    placeholder="hoge@gmail.com"
-                    type="email"
-                    required
-                    :rules="[validation.required, validation.checkEmail]"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-card-title>性別</v-card-title>
-              <v-row>
-                <v-col cols="12">
-                  <v-radio-group
-                    v-model="gender"
-                    inline
-                    required
-                    :rules="[validation.required]"
-                  >
-                    <v-radio label="女性" value="1"></v-radio>
-                    <v-radio label="男性" value="0"></v-radio>
-                    <v-radio label="回答しない" value="2"></v-radio>
-                  </v-radio-group>
-                </v-col>
-              </v-row>
-              <v-card-title>学籍番号</v-card-title>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    v-model.trim="studentID"
-                    label="７桁の半角数字を入力してください"
-                    placeholder="1234567"
-                    :rules="[validation.required]"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12" xl="12" sm="3" md="2" lg="2" class="mx-auto">
-                  <v-btn size="large" type="submit" color="success" block>
-                    確認
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <main class="surface-ground w-screen min-h-screen p-3">
+    <Card class="bg-white shadow-none w-95 m-auto mt-2 border-round-lg">
+      <template #title>ボタンギャラリー</template>
+      <template #content>
+        <div class="flex gap-3">
+          <Button label="ボタン" icon="pi pi-check" />
+          <Button type="button" label="Button 1"></Button>
+          <Button type="button" label="Button 2" severity="secondary"></Button>
+          <Button type="button" label="Button 3" severity="help"></Button>
+        </div>
+      </template>
+    </Card>
+    <Card class="bg-white shadow-none w-95 m-auto mt-2 border-round-lg">
+      <template #title>フォームギャラリー</template>
+      <template #content>
+        <InputGroup class="flex gap-4">
+          <FloatLabel class="w-20rem">
+            <InputText v-model="firstName" type="text" />
+            <label for="username">姓</label>
+          </FloatLabel>
+          <FloatLabel class="w-20rem">
+            <InputText v-model="lastName" type="text" />
+            <label for="username">名</label>
+          </FloatLabel>
+        </InputGroup>
+        <InputGroup class="flex mt-4 gap-4">
+          <FloatLabel class="w-20rem">
+            <InputText v-model="firstNameKana" type="text" />
+            <label for="username">セイ</label>
+          </FloatLabel>
+          <FloatLabel class="w-20rem">
+            <InputText v-model="lastNameKana" type="text" />
+            <label for="username">メイ</label>
+          </FloatLabel>
+        </InputGroup>
+        <FloatLabel class="mt-4">
+          <InputText v-model="email" type="email" class="w-30rem" />
+          <label for="username">メールアドレス</label>
+        </FloatLabel>
+        <InputGroup class="mt-4">
+          <div>
+            <RadioButton
+              v-model="gender"
+              inputId="gender1"
+              name="gender"
+              value="0"
+            />
+            <label for="gender1" class="p-2">男性</label>
+          </div>
+          <div>
+            <RadioButton
+              v-model="gender"
+              inputId="gender2"
+              name="gender"
+              value="1"
+            />
+            <label for="gender2" class="p-2">女性</label>
+          </div>
+        </InputGroup>
+        <FloatLabel class="mt-4">
+          <InputNumber
+            v-model="studentID"
+            inputId="student-id"
+            :useGrouping="false"
+            mode="decimal"
+          />
+          <label for="student-id">学籍番号</label>
+        </FloatLabel>
+        <div class="card flex justify-content-center">
+          <Listbox
+            v-model="selectedCity"
+            :options="cities"
+            optionLabel="name"
+            class="w-full md:w-14rem"
+          />
+        </div>
+        <Checkbox v-model="checked" :binary="true" />
+      </template>
+    </Card>
+  </main>
 </template>
