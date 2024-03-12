e2cc175f45aa9f662b2da9c7fd7bfec851e281eb
tomoyahiroe <tomoya4creative@gmail.com>
2024-03-12 11:33:33 +0900

    create: PageTitle Component

---
+++ components/PageTitle.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+  pageTitle: {
+    type: String,
+    required: true
+  },
+  backLink: {
+    type: String,
+    default: '/'
+  }
+})
+</script>
+
+<template>
+  <h1>{{ props.pageTitle }}</h1>
+</template>
+
+<style scoped></style>
