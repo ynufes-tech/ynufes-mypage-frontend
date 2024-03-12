3a0b32247fa2fd82384a2120748e0338eaa07fb4
tomoyahiroe <tomoya4creative@gmail.com>
2024-03-12 13:08:58 +0900

    :recycle: Refactor PageTitle component to include back link and improve styling

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -5,14 +5,24 @@ const props = defineProps({
     required: true
   },
   backLink: {
-    type: String,
-    default: '/'
+    type: String
   }
 })
 </script>
 
 <template>
-  <h1>{{ props.pageTitle }}</h1>
+  <div class="page-title">
+    <a v-if="props.backLink" class="util-color-text" :href="props.backLink">
+      <i class="pi pi-angle-left text-xl mr-1 font-bold inline"></i>
+    </a>
+    <i
+      v-if="!props.backLink"
+      class="pi pi-angle-right text-xl mr-1 font-bold inline"
+    ></i>
+    <h1 class="text-xl font-bold inline vertical-align-text-top">
+      {{ props.pageTitle }}
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
