277d85377d96312dbefd08ef0109a224f4367d91
tomoyahiroe <tomoya4creative@gmail.com>
2024-03-13 15:01:33 +0900

    💄 add: page animation NuxtLink to NuxtLink

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,8 @@ export default defineNuxtConfig({
       bodyAttrs: {
         class: 'test'
       }
-    }
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
   },
   typescript: {
     shim: false,
