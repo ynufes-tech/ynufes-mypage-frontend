38d88ba09afb547a28d0c12bae922762aff06b5d
tomoyahiroe <tomoya4creative@gmail.com>
2024-03-13 15:11:47 +0900

    💄 add animation login and welcome page

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -165,3 +165,15 @@ const user = await useLogin().getCurrentUser()
     </div>
   </main>
 </template>
+<style scoped lang="scss">
+// page transition のアニメーション
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
