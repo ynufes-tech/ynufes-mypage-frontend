33c395ebcf113a5dac34000e8dd2fa66618f012f
Shion Ichikawa <shion1305@gmail.com>
2024-05-05 09:28:23 +0900

    ✨ Setup Vueform

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,7 @@ export default defineNuxtConfig({
   modules: [
     'nuxt-primevue',
     '@pinia/nuxt',
+    '@vueform/nuxt',
     [
       '@nuxtjs/google-fonts',
       {
---
+++ vueform.config.ts
@@ -0,0 +1,14 @@
+// vueform.config.(js|ts)
+
+import en from '@vueform/vueform/locales/en'
+import vueform from '@vueform/vueform/dist/vueform'
+import { defineConfig } from '@vueform/vueform'
+
+// You might place these anywhere else in your project
+import '@vueform/vueform/dist/vueform.css'
+
+export default defineConfig({
+  theme: vueform,
+  locales: { en },
+  locale: 'en'
+})
